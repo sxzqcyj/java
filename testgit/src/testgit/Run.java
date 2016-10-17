2ee3efca5f4c052b5c49501b24be3b653bb55be6
cyj-pc <cyj-pc@cyj-pro4>
2016-10-17 10:13:16 +0800

    first

---
+++ testgit/src/testgit/Run.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package testgit;
+
+/**
+ * @author cyj-pc
+ *
+ */
+public class Run {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("cyj");
+	}
+
+}
